c3540c3422a2e8df580a31ab80cedea05d733618
Jack McLovin <aden.handasyde@utas.edu.au>
2024-12-23 13:34:14 +0800

    auto

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,7 @@ STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
 SECRET_KEY = env('SECRET_KEY')
 OPEN_AI_API_KEY = env("OPEN_AI_API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 COINBASE_COMMERCE_API_KEY = env('COINBASE_COMMERCE_API_KEY')
 
